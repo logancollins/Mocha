be7825e62efc408fa1b410f6a9d4d0bcedbee2c1
Logan Collins <loganscollins@gmail.com>
2012-05-12 18:33:15 -0700

    Fixed path issue.

---
+++ Mocha/Categories/NSArray+MochaAdditions.m
@@ -0,0 +1,32 @@
+//
+//  NSArray+MochaAdditions.m
+//  Mocha
+//
+//  Created by Logan Collins on 5/12/12.
+//  Copyright (c) 2012 Sunflower Softworks. All rights reserved.
+//
+
+#import "NSArray+MochaAdditions.h"
+
+
+@implementation NSArray (MochaAdditions)
+
+- (id)mo_objectForIndexedSubscript:(NSUInteger)idx {
+    return [self objectAtIndex:idx];
+}
+
+@end
+
+
+@implementation NSMutableArray (MochaAdditions)
+
+- (void)mo_setObject:(id)obj forIndexedSubscript:(NSUInteger)idx {
+    if ([self count] > idx) {
+        [self replaceObjectAtIndex:idx withObject:obj];
+    }
+    else if ([self count] == idx) {
+        [self addObject:obj];
+    }
+}
+
+@end
