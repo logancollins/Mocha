88b1cb4f38f486b8955033b8058dcf25ea417180
Logan Collins <loganscollins@gmail.com>
2012-06-13 09:20:18 -0700

    Updated documentation in headers.

--- Mocha/Objects/MOInstanceVariableDescription.h
+++ Mocha/Objects/MOInstanceVariableDescription.h
@@ -15,9 +15,35 @@
  */
 @interface MOInstanceVariableDescription : NSObject
 
+/*!
+ * @method instanceVariableWithName:typeEncoding:
+ * @abstract Creates a new instance variable
+ * 
+ * @param name
+ * The name of the instance variable
+ * 
+ * @param typeEncoding
+ * The type encoding of the instance variable
+ * 
+ * @result An MOInstanceVariableDescription object
+ */
 + (MOInstanceVariableDescription *)instanceVariableWithName:(NSString *)name typeEncoding:(NSString *)typeEncoding;
 
+
+/*!
+ * @property name
+ * @abstract The name of the instance variable
+ * 
+ * @result An NSString object
+ */
 @property (copy, readonly) NSString *name;
+
+/*!
+ * @property typeEncoding
+ * @abstract The type encoding of the instance variable
+ * 
+ * @result An NSString object
+ */
 @property (copy, readonly) NSString *typeEncoding;
 
 @end
--- Mocha/Objects/MOMethodDescription.h
+++ Mocha/Objects/MOMethodDescription.h
@@ -15,9 +15,34 @@
  */
 @interface MOMethodDescription : NSObject
 
+/*!
+ * @method methodWithSelector:typeEncoding:
+ * @abstract Creates a new method
+ * 
+ * @param selector
+ * The selector of the method
+ * 
+ * @param typeEncoding
+ * The type encoding of the method
+ * 
+ * @result An MOMethodDescription object
+ */
 + (MOMethodDescription *)methodWithSelector:(SEL)selector typeEncoding:(NSString *)typeEncoding;
 
+/*!
+ * @property selector
+ * @abstract The selector of the method
+ * 
+ * @result A SEL value
+ */
 @property (readonly) SEL selector;
+
+/*!
+ * @property typeEncoding
+ * @abstract The type encoding of the method
+ * 
+ * @result An NSString object
+ */
 @property (copy, readonly) NSString *typeEncoding;
 
 @end
